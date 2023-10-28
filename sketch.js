d7a0695b5ab9a015cc0fcbe742354ba38b67651f
happy2718 <xgao0429@uni.sydney.edu.au>
2023-10-28 12:53:52 +1100

    Introduce the MovingCircle class and create a moving instance of the circle

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
+let movingCircle;
 function setup() {
   createCanvas(1000, 600);
+  movingCircle = new MovingCircle();
 }
 
 function draw() {
-  movingSpeed = map(mouseX, 0, width, 0, 100);
   background(0);
-  fill(255);
-  noStroke();
-  ellipse(movingSpeed, height / 2, 50, 50);
+  movingCircle.update();
+  movingCircle.display();
+}
+
+class MovingCircle {
+  constructor() {
+    this.xPosition = width / 2;
+    this.yPosition = height / 2;
+    this.circleRadius = 50;
+  }
+
+  update() {
+    this.xPosition = mouseX;
+  }
+
+  display() {
+    fill(255);
+    noStroke();
+    ellipse(this.xPosition, this.yPosition, this.circleRadius, this.circleRadius);
+  }
 }
